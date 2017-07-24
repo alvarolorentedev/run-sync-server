565f49ad8e0ba0a1f8ad0b62c673de2980dbd4e3
alvaro <alvarojosepl@gmail.com>
2017-07-24 08:30:10 +0200

    restart the project as per decisitions of other day

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 3080;
+const app = express();
+
+app.use(helmet());
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+  next();
+});
+
+// app.use('/accounts', require('./src/accounts/router'));
+
+app.listen(port);
--- test/users/service.spec.js
+++
@@ -1,7 +0,0 @@
-import { expect } from 'chai';
-
-xdescribe('User Service', () => {
-  it('should fail', () => {
-    expect(1).to.equal(2);
-  });
-});
