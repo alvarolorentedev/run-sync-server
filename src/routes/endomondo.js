46544ec387fe93467fe43714cd11d2b259c0fe36
alvaro <alvarojosepl@gmail.com>
2017-07-31 08:24:55 +0200

    added implementation of endomondo workouts

--- src/routes/endomondo.js
+++ src/routes/endomondo.js
@@ -2,8 +2,22 @@ const router = require('express').Router()
 const endomondo = require('endomondo-unofficial-api')
 
 router.post('/login', function(req, res) {
+    if(!req.body.email || !req.body.password)
+        res.sendStatus(400)
     endomondo.authenticate(req.body).then(
         result => res.send(result)
+    ).catch(
+        err => res.sendStatus(400)
+    )
+})
+
+router.post('/workouts', function(req, res) {
+    if(!req.body.token)
+        res.sendStatus(400)
+    endomondo.workouts(req.body).then(
+        result => res.send(result)
+    ).catch(
+        err => res.sendStatus(400)
     )
 })
 
--- test/routes/endomondo-test.js
+++ test/routes/endomondo-test.js
@@ -1,5 +1,6 @@
 jest.mock('endomondo-unofficial-api', () => ({
-  authenticate: jest.fn()
+  authenticate: jest.fn(),
+  workouts : jest.fn()
 }));
 
 const endomondo = require('endomondo-unofficial-api')
@@ -14,7 +15,9 @@ const request = require('supertest')(app)
 describe('Test the endomondo endpoit', () => {
     beforeEach(() => {
         endomondo.authenticate.mockReset()
+        endomondo.workouts.mockReset()
         endomondo.authenticate.mockImplementation(() => { return Promise.resolve( { works: true } ) })
+        endomondo.workouts.mockImplementation(() => { return Promise.resolve( [ { works: true } ] ) })
     })
 
     test('endomondo login is called with correct parameters', () => {
@@ -24,6 +27,58 @@ describe('Test the endomondo endpoit', () => {
             expect(endomondo.authenticate.mock.calls.length).toBe(1)
             expect(endomondo.authenticate.mock.calls[0][0].email).toBe(randomEmail)
             expect(endomondo.authenticate.mock.calls[0][0].password).toBe(randomPassword)
+        })
+    })
+
+    test('endomondo login returns error if empty request', () => {
+        endomondo.authenticate.mockImplementation(() => { throw "exception" })
+        return request.post('/endomondo/login').send().then(result => {
+            expect(result.statusCode).toBe(400)
+        })
+    })
+
+    test('endomondo login returns correct response if service resolves', () => {
+        return request.post('/endomondo/login').send({email:'pepe',password:'perez'}).then(result => {
+            expect(result.body).toEqual({ works: true })
+            expect(result.statusCode).toBe(200)
+        })
+    })
+
+    test('endomondo login returns error if incorrect service rejects', () => {
+        endomondo.authenticate.mockImplementation(() => { return Promise.reject() })
+        return request.post('/endomondo/login').send({email:'pepe',password:'perez'}).then(result => {
+            expect(endomondo.authenticate.mock.calls.length).toBe(1)
+            expect(result.statusCode).toBe(400)
+        })
+    })
+
+    test('endomondo get list of workouts with correct parameters', () => {
+        var randomToken = Math.random().toString(36).substr(0, 5)
+        return request.post('/endomondo/workouts').send({token: randomToken}).then(result => {
+            expect(endomondo.workouts.mock.calls.length).toBe(1)
+            expect(endomondo.workouts.mock.calls[0][0].token).toBe(randomToken)
+            expect(result.statusCode).toBe(200)
+        })
+    })
+
+    test('endomondo get list of workouts returns correct result', () => {
+        return request.post('/endomondo/workouts').send({token: 'pepe'}).then(result => {
+            expect(result.body).toEqual([ { works: true } ])
+            expect(result.statusCode).toBe(200)
+        })
+    })
+
+    test('endomondo get list of workouts returns error if empty request', () => {
+        return request.post('/endomondo/workouts').send().then(result => {
+            expect(result.statusCode).toBe(400)
+        })
+    })
+
+    test('endomondo get list of workouts returns error if incorrect service rejects', () => {
+        endomondo.workouts.mockImplementation(() => { return Promise.reject() })
+        return request.post('/endomondo/workouts').send({token: 'pepe'}).then(result => {
+            expect(endomondo.workouts.mock.calls.length).toBe(1)
+            expect(result.statusCode).toBe(400)
         })
     })
 })
