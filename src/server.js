9ec8b0d7a049c778905b862de0d7e13fcd43933e
alvaro <alvarojosepl@gmail.com>
2017-07-31 08:37:46 +0200

    added cors middleware for origen and request control

--- src/server.js
+++ src/server.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const helmet = require('helmet');
-const bodyParser = require('body-parser');
+const express = require('express')
+const helmet = require('helmet')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-const port = process.env.PORT || 3080;
-const app = express();
+const port = process.env.PORT || 3080
+const app = express()
 
-app.use(helmet());
-app.use(bodyParser.json());
+app.use(helmet())
+app.use(bodyParser.json())
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
-  next();
-});
+app.use(cors({
+  origin: true,
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}))
 
-app.use('/nike', require('./routes/nike'));
-app.use('/endomondo', require('./routes/endomondo'));
+app.use('/nike', require('./routes/nike'))
+app.use('/endomondo', require('./routes/endomondo'))
 
-app.listen(port);
+app.listen(port)
