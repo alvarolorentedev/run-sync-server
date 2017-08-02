7f1d79625309e0641949863c078777d71f6803d3
alvaro <alvarojosepl@gmail.com>
2017-08-02 09:04:18 +0200

    implemented endomundo using async await

--- src/routes/endomondo.js
+++ src/routes/endomondo.js
@@ -1,24 +1,52 @@
 const router = require('express').Router()
 const endomondo = require('endomondo-unofficial-api')
 
-router.post('/login', function(req, res) {
-    if(!req.body.email || !req.body.password)
+router.post('/login', async (req, res) => {
+    try {
+        if(!req.body.email || !req.body.password)
+            res.sendStatus(400)
+        let result = await endomondo.authenticate(req.body)
+        res.send(result)
+    } 
+    catch (err) {
         res.sendStatus(400)
-    endomondo.authenticate(req.body).then(
-        result => res.send(result)
-    ).catch(
-        err => res.sendStatus(400)
-    )
+    }
 })
 
-router.get('/workouts', function(req, res) {
-    if(!req.body.token)
+router.get('/workouts', async (req, res) => {
+    try {
+        if(!req.body.authToken)
+            res.sendStatus(400)
+        let result = await endomondo.workouts(req.body)
+        res.send(result)
+    } 
+    catch (err) {
         res.sendStatus(400)
-    endomondo.workouts(req.body).then(
-        result => res.send(result)
-    ).catch(
-        err => res.sendStatus(400)
-    )
+    }
+    
 })
+
+router.post('/workouts', async (req, res) => {
+    try {
+        let uploads = req.body.map(upload)
+        await Promise.all(uploads)
+        res.sendStatus(200)
+    } 
+    catch (error) {
+        res.sendStatus(400)
+    }
+})
+
+async function upload(run){
+    try {
+        if(!run.authToken || !run.userId || !run.duration || !run.time)
+            return Promise.reject()
+        let result = await endomondo.workout.set(run)
+        return Promise.resolve(result)
+    }
+    catch(err) {
+        return Promise.reject()
+    }
+}
 
 module.exports = router;
