be103abcbd5682cdef491ec657a3340e247775eb
alvaro <alvarojosepl@gmail.com>
2017-08-04 09:05:05 +0200

    reenabled tests and updated travis.yml

--- test/routes/endomondo-test.js
+++ test/routes/endomondo-test.js
@@ -56,71 +56,71 @@ describe('Test the endomondo endpoit', () => {
         expect(result.statusCode).toBe(500)
     })
 
-    // test('endomondo get list of workouts with correct parameters', async () => {
-    //     let randomToken = Math.random().toString(36).substr(0, 5)
-    //     let result = await request.get('/endomondo/workouts').send({authToken: randomToken})
-    //     expect(endomondo.workouts.mock.calls.length).toBe(1)
-    //     expect(endomondo.workouts.mock.calls[0][0].authToken).toBe(randomToken)
-    //     expect(result.statusCode).toBe(200)
-    // })
+    test('endomondo get list of workouts with correct parameters', async () => {
+        let randomToken = Math.random().toString(36).substr(0, 5)
+        let result = await request.get('/endomondo/workouts').send({authToken: randomToken})
+        expect(endomondo.workouts.mock.calls.length).toBe(1)
+        expect(endomondo.workouts.mock.calls[0][0].authToken).toBe(randomToken)
+        expect(result.statusCode).toBe(200)
+    })
 
-    // test('endomondo get list of workouts returns correct result', async () => {
-    //     let result = await request.get('/endomondo/workouts').send({authToken: 'pepe'})
-    //     expect(result.body).toEqual([ { works: true } ])
-    //     expect(result.statusCode).toBe(200)
-    // })
+    test('endomondo get list of workouts returns correct result', async () => {
+        let result = await request.get('/endomondo/workouts').send({authToken: 'pepe'})
+        expect(result.body).toEqual([ { works: true } ])
+        expect(result.statusCode).toBe(200)
+    })
 
-    // test('endomondo get list of workouts returns error if empty request', async () => {
-    //     let result = await request.get('/endomondo/workouts').send()
-    //     expect(result.statusCode).toBe(400)
-    // })
+    test('endomondo get list of workouts returns error if empty request', async () => {
+        let result = await request.get('/endomondo/workouts').send()
+        expect(result.statusCode).toBe(400)
+    })
 
-    // test('endomondo get list of workouts returns error if incorrect service rejects', async () => {
-    //     endomondo.workouts.mockImplementation(() => { return Promise.reject() })
-    //     let result = await request.get('/endomondo/workouts').send({authToken: 'pepe'})
-    //     expect(endomondo.workouts.mock.calls.length).toBe(1)
-    //     expect(result.statusCode).toBe(500)
-    // })
+    test('endomondo get list of workouts returns error if incorrect service rejects', async () => {
+        endomondo.workouts.mockImplementation(() => { return Promise.reject() })
+        let result = await request.get('/endomondo/workouts').send({authToken: 'pepe'})
+        expect(endomondo.workouts.mock.calls.length).toBe(1)
+        expect(result.statusCode).toBe(500)
+    })
 
-    // test('endomondo post calls workouts with correct parameters', async () => {
-    //     let randomToken = Math.random().toString(36).substr(0, 5)
-    //     let randomUserId = Math.random().toString(36).substr(0, 5)
-    //     let randomDuration = Math.random()
-    //     let randomTime = Math.random()
-    //     let result = await request.post('/endomondo/workouts').send([{authToken: randomToken, userId : randomUserId, duration: randomDuration , time : randomTime}])
-    //     expect(endomondo.workout.set.mock.calls.length).toBe(1)
-    //     expect(endomondo.workout.set.mock.calls[0][0].authToken).toBe(randomToken)
-    //     expect(endomondo.workout.set.mock.calls[0][0].duration).toBe(randomDuration)
-    //     expect(endomondo.workout.set.mock.calls[0][0].time).toBe(randomTime)
-    //     expect(result.statusCode).toBe(200)
-    // })
+    test('endomondo post calls workouts with correct parameters', async () => {
+        let randomToken = Math.random().toString(36).substr(0, 5)
+        let randomUserId = Math.random().toString(36).substr(0, 5)
+        let randomDuration = Math.random()
+        let randomTime = Math.random()
+        let result = await request.post('/endomondo/workouts').send([{authToken: randomToken, userId : randomUserId, duration: randomDuration , time : randomTime}])
+        expect(endomondo.workout.set.mock.calls.length).toBe(1)
+        expect(endomondo.workout.set.mock.calls[0][0].authToken).toBe(randomToken)
+        expect(endomondo.workout.set.mock.calls[0][0].duration).toBe(randomDuration)
+        expect(endomondo.workout.set.mock.calls[0][0].time).toBe(randomTime)
+        expect(result.statusCode).toBe(200)
+    })
 
-    // test('endomondo post workouts cause a reject', async () => {
-    //     endomondo.workout.set.mockReset()
-    //     endomondo.workout.set.mockImplementation(() => { return Promise.reject() })
-    //     let result = await request.post('/endomondo/workouts').send([{authToken: "pepe", userId : "pepe", duration: 10, time : 10}])
-    //     expect(endomondo.workout.set.mock.calls.length).toBe(1)
-    //     expect(result.statusCode).toBe(500)
-    // })
+    test('endomondo post workouts cause a reject', async () => {
+        endomondo.workout.set.mockReset()
+        endomondo.workout.set.mockImplementation(() => { return Promise.reject() })
+        let result = await request.post('/endomondo/workouts').send([{authToken: "pepe", userId : "pepe", duration: 10, time : 10}])
+        expect(endomondo.workout.set.mock.calls.length).toBe(1)
+        expect(result.statusCode).toBe(500)
+    })
 
-    // test('endomondo post calls without authToken', async () => {
-    //     let randomToken = Math.random().toString(36).substr(0, 5)
-    //     let result = await request.post('/endomondo/workouts').send([{userId: "pepe", duration: 10, time : 10}])
-    //     expect(result.statusCode).toBe(400)
-    // })
+    test('endomondo post calls without authToken', async () => {
+        let randomToken = Math.random().toString(36).substr(0, 5)
+        let result = await request.post('/endomondo/workouts').send([{userId: "pepe", duration: 10, time : 10}])
+        expect(result.statusCode).toBe(400)
+    })
 
-    // test('endomondo post calls without userId', async () => {
-    //     let result = await request.post('/endomondo/workouts').send([{authToken: "pepe", duration: 10, time : 10}])
-    //     expect(result.statusCode).toBe(400)
-    // })
+    test('endomondo post calls without userId', async () => {
+        let result = await request.post('/endomondo/workouts').send([{authToken: "pepe", duration: 10, time : 10}])
+        expect(result.statusCode).toBe(400)
+    })
 
-    // test('endomondo post calls without duration', async () => {
-    //     let result = await request.post('/endomondo/workouts').send([{authToken: "pepe", userId: "pepe", time : 10}])
-    //     expect(result.statusCode).toBe(400)
-    // })
+    test('endomondo post calls without duration', async () => {
+        let result = await request.post('/endomondo/workouts').send([{authToken: "pepe", userId: "pepe", time : 10}])
+        expect(result.statusCode).toBe(400)
+    })
 
-    // test('endomondo post calls without time', async () => {
-    //     let result = await request.post('/endomondo/workouts').send([{authToken: "pepe", userId: "pepe" , duration: 10}])
-    //     expect(result.statusCode).toBe(400)
-    // })
+    test('endomondo post calls without time', async () => {
+        let result = await request.post('/endomondo/workouts').send([{authToken: "pepe", userId: "pepe" , duration: 10}])
+        expect(result.statusCode).toBe(400)
+    })
 })
